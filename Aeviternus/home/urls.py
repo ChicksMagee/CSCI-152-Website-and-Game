08a21c965c4d5959317ff3deca5e607227e88506
ChicksMagee <tesamra@gmail.com>
2018-04-09 12:12:27 -0700

    Removed Harcdoded urls in the templates
    make the urls more dynamic

--- Aeviternus/home/urls.py
+++ Aeviternus/home/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home),
-    url(r'^about/$', views.about),
-    url(r'^buy-now/$', views.buyNow),
-    url(r'^login/$', views.login),
+    url(r'^$', views.home, name='home'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^buy-now/$', views.buyNow, name='buy-now'),
+    url(r'^login/$', views.login, name='login'),
 ]
