e8c997047cd589794e8cbaf5ce630d25eda66c26
ChicksMagee <tesamra@gmail.com>
2018-04-26 15:39:20 -0700

    Added more Models
    Creating the Contact Form Model

--- Aeviternus/Aeviternus/urls.py
+++ Aeviternus/Aeviternus/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from home import views
 urlpatterns = [
+    path(r'', views.home),
     path('admin/', admin.site.urls),
     path(r'home/', include('home.urls')),
 ]
--- Aeviternus/home/models.py
+++ Aeviternus/home/models.py
@@ -16,3 +16,6 @@ def create_profile(sender, **kwargs):
 
 
 post_save.connect(create_profile, sender = User)
+
+
+#class ContactForm(models.Model):
