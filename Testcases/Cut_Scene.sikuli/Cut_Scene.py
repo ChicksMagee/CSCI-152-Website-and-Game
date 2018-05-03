f142f5fbba164e9f7e07bfbb6bc1a62692e6a534
Harbhajan SIngh <singharbhajanhs@mail.fresnostate.edu>
2018-05-02 23:46:12 -0700

    More cutscenes

---
+++ Testcases/Cut_Scene.sikuli/Cut_Scene.py
@@ -0,0 +1,29 @@
+import time
+wait(5)
+type("dd")
+type(" ")
+wait(2)
+type("dd")
+type(" ")
+wait(2)
+type("dd")
+type(" ")
+wait(2)
+type("dd")
+type(" ")
+wait(2)
+doubleClick("1525160003991.png")
+wait(5)
+type("dddddd")
+wait(17)
+
+
+doubleClick("1525160065073.png")
+
+wait(17)
+doubleClick("1525160566207.png")
+doubleClick("1525160566207.png")
+doubleClick("1525160566207.png")
+doubleClick("1525160566207.png")
+
+
